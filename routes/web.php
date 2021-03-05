50a153956f2b6918bc67a6ed3f2fc8de429f386f
JUNMAR77 <junmar77@gmail.com>
2021-03-05 22:40:44 +0800

    Added tpc logo

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
